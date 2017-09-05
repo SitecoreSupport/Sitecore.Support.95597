3ff0c6f03f07cbd759251af41e51324598841f58
Vladyslav Molibozhenko <vlm@sitecore.net>
2017-09-05 17:39:47 +0300

    Fix for Bucket Search returning items only in content language was added

---
+++ src/Sitecore.Support.95597/Buckets/Pipelines/DisableLanguageFilterProcessor.cs
@@ -0,0 +1,23 @@
+﻿using Sitecore.Buckets.Pipelines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Buckets.Pipelines.UI.Search;
+using Sitecore.Configuration;
+using Sitecore.ContentSearch;
+using Sitecore.Data.Items;
+using Sitecore.Globalization;
+
+namespace Sitecore.Support.Buckets.Pipelines
+{
+    public class DisableLanguageFilterProcessor : BucketsPipelineProcessor<UISearchArgs>
+    {
+        public override void Process(UISearchArgs args)
+        {
+            Item item = args.StartLocation.Item.Database.GetItem(args.StartLocation.Item.ID, Language.Parse(Settings.DefaultLanguage));
+            args.StartLocation = new SitecoreIndexableItem(item);
+        }
+
+    }
+}
